8f56d310a76e658be9358ae68cad1f1aa3f8e1e2
FRySnz <60758019+FRySnz@users.noreply.github.com>
2022-08-10 17:33:56 -0500

    Update child_reference_ei.R

--- R/child_reference_ei.R
+++ R/child_reference_ei.R
@@ -94,6 +94,11 @@ child_reference_EI <- function(age, sex, bmiCat, FM, FFM, days, dt = 1){
                    " instead."))
   }
   
+    #Check bmiCat is 1-4
+  if (!(bmiCat %in% c(1,2,3,4))){
+    stop("Invalid bmi category value (bmiCat). Please specify 1 for underweight, 2 for normal weight, 3 for overweight or 4 for obesity.")
+  }
+  
   #Check that dt is > 0
   if (dt < 0 || dt > days){
     stop(paste0("Invalid time step dt; please choose 0 < dt < days"))
